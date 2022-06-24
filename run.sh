5bc8a1ad168f005b832a4f359852c195b3a9a7ce
peterhpchen <peterhsinpingchen@gmail.com>
2022-06-24 16:37:34 +0800

    rename

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --progress=plain -t sunny .
-docker run -v "$(pwd)":"/root/workspace" -it sunny
+docker build --progress=plain -t sunnyvim .
+docker run -v "$(pwd)":"/root/workspace" -it sunnyvim
