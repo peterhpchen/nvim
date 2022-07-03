afa09eac692d00a37f96dcf9c2ca7ed3d5bbca9a
peterhpchen <peterhsinpingchen@gmail.com>
2022-07-03 13:03:39 +0800

    config move

---
+++ xdg-config-home/nvim/lua/svim/init.lua
@@ -0,0 +1,29 @@
+require('plugins')
+
+vim.api.nvim_set_keymap('n', '<Leader>x', ':bd<CR>', { noremap = true, silent = true })
+
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- indenting
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+vim.opt.showmode = false
+
+vim.opt.clipboard = 'unnamedplus'
+
+-- diagnostic
+vim.diagnostic.config({ severity_sort = true })
+local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+end
