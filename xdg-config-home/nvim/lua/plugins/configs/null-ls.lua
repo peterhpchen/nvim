07bc540bc80ce6e3d0139b88d330b009d2b0c561
peterhpchen <peterhsinpingchen@gmail.com>
2022-07-06 15:34:45 +0800

    nullls dpkg setting

--- xdg-config-home/nvim/lua/plugins/configs/null-ls.lua
+++ xdg-config-home/nvim/lua/plugins/configs/null-ls.lua
@@ -10,18 +10,27 @@ if not present2 then
   return
 end
 
+local sources = {
+  -- Dockerfile
+  null_ls.builtins.diagnostics.hadolint,
+
+  -- Shell
+  null_ls.builtins.code_actions.shellcheck,
+  null_ls.builtins.diagnostics.shellcheck,
+  null_ls.builtins.formatting.shfmt,
+}
+
+local config = require('svim/core/default-config')
+for _, ext in pairs(config.extensions) do
+  local ext_sources_exists, ext_sources = pcall(require, ext .. '/plugins/configs/null-ls')
+  if ext_sources_exists then
+    for _, ext_source in pairs(ext_sources) do
+      table.insert(sources, ext_source)
+    end
+  end
+end
+
 null_ls.setup({
   on_attach = on_attach,
-  sources = {
-    -- Dockerfile
-    null_ls.builtins.diagnostics.hadolint,
-
-    -- Lua
-    null_ls.builtins.formatting.stylua,
-
-    -- Shell
-    null_ls.builtins.code_actions.shellcheck,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.formatting.shfmt,
-  },
+  sources = sources,
 })
