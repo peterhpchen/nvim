d8c379839c4f2b2af6e6811e97bd4ce4c6f23dcb
peterhpchen <peterhsinpingchen@gmail.com>
2022-07-12 08:21:53 +0800

    individual options

---
+++ lua/svim/core/options.lua
@@ -0,0 +1,19 @@
+local opt = vim.opt
+
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = 'yes'
+
+opt.ignorecase = true
+opt.smartcase = true
+
+-- indenting
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+
+opt.showmode = false
+
+opt.clipboard = 'unnamedplus'
--- lua/svim/init.lua
+++ lua/svim/init.lua
@@ -1,24 +1,7 @@
-require('svim/plugins')
+require('svim.core.options')
+require('svim.plugins')
 
 vim.api.nvim_set_keymap('n', '<Leader>x', ':bd<CR>', { noremap = true, silent = true })
-
-vim.opt.cursorline = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = 'yes'
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- indenting
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.smartindent = true
-
-vim.opt.showmode = false
-
-vim.opt.clipboard = 'unnamedplus'
 
 -- diagnostic
 vim.diagnostic.config({ severity_sort = true })
