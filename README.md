70ae3d7b7a75c3a11bfd67151d10f7ed5f1ab5c8
peterhpchen <peterhsinpingchen@gmail.com>
2022-07-31 18:16:19 +0800

    feat(install): full install script

---
+++ README.md
@@ -0,0 +1,19 @@
+# Peter's Neovim
+
+## Installation
+
+Install this config by the below command:
+
+```sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/sunnyvim/sunnyvim/main/utils/installer/install.sh)"
+```
+
+## Uninstall
+
+Just execute the following command:
+
+```sh
+rm -rf ~/.config/nvim \
+~/.local/share/nvim \
+~/.cache/nvim
+```
--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -9,3 +9,23 @@ if [ ! "$(command -v nvim)" ]; then
   echo "Please install Neovim first."
   exit 1
 fi
+
+if [ ! "$(command -v hadolint)" ]; then
+  echo "Please install hadolint first."
+  exit 1
+fi
+
+if [ ! "$(command -v shellcheck)" ]; then
+  echo "Please install ShellCheck first."
+  exit 1
+fi
+
+if [ ! "$(command -v shfmt)" ]; then
+  echo "Please install shfmt first."
+  exit 1
+fi
+
+if [ ! "$(command -v stylua)" ]; then
+  echo "Please install StyLua first."
+  exit 1
+fi
