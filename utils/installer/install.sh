3f8a1166a42f2ce1210904d4af7a9312aab5c625
peterhpchen <peterhsinpingchen@gmail.com>
2022-08-12 08:14:40 +0800

    feat(install): copy config from remote

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+NVIM_CONFIG_DIR="$XDG_CONFIG_HOME/nvim"
+REMOTE=https://github.com/peterhpchen/nvim.git
 
 if [ ! "$(command -v git)" ]; then
   echo "Please install Git first."
@@ -11,6 +15,13 @@ if [ ! "$(command -v nvim)" ]; then
   echo "Please install Neovim first."
   exit 1
 fi
+
+if [ -d "$NVIM_CONFIG_DIR" ]; then
+  echo "The $NVIM_CONFIG_DIR folder already exists. Please remove it."
+  exit 1
+fi
+
+git clone --quiet "$REMOTE" "$NVIM_CONFIG_DIR"
 
 if [ ! -d "$XDG_DATA_HOME/nvim/site/pack/packer/start/packer.nvim" ]; then
   echo "packer.nvim not installed. Installing."
