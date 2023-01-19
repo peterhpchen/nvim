b6a04cd5761fa565f81f43660b01211e56a83fc6
peterhpchen <peterhsinpingchen@gmail.com>
2023-01-19 12:48:00 +0800

    chore(lspsaga): breaking change

--- lua/plugins/configs/lspsaga.lua
+++ lua/plugins/configs/lspsaga.lua
@@ -4,6 +4,8 @@ if not lspsaga_ok then
   return
 end
 
+saga.setup({})
+
 vim.api.nvim_set_keymap('n', 'gh', ':Lspsaga lsp_finder<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ca', ':Lspsaga code_action<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>ca', ':<C-U>Lspsaga range_code_action<CR>', { noremap = true, silent = true })
@@ -14,5 +16,3 @@ vim.api.nvim_set_keymap('n', 'gd', ':Lspsaga preview_definition<CR>', { noremap 
 vim.api.nvim_set_keymap('n', 'K', ':Lspsaga hover_doc<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '[e', ':Lspsaga diagnostic_jump_next<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', ']e', ':Lspsaga diagnostic_jump_prev<CR>', { noremap = true, silent = true })
-
-saga.init_lsp_saga()
