01ff2bb83838b37eec61b41f013f1a9b1097eaa3
peterhpchen <peterhsinpingchen@gmail.com>
2023-02-03 12:40:57 +0800

    refactor(plugin): move to after folder for auto load

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,13 +8,7 @@ vim.cmd('packadd packer.nvim')
 packer.startup(function(use)
   use('wbthomason/packer.nvim')
 
-  use({
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    config = function()
-      require('plugins.configs.treesitter')
-    end,
-  })
+  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
   use('JoosepAlviste/nvim-ts-context-commentstring')
 
   -- Lua
@@ -25,21 +19,13 @@ packer.startup(function(use)
   -- disable because bug issue https://github.com/vuki656/package-info.nvim/issues/127
   -- use({
   --   'vuki656/package-info.nvim',
-  --   config = function()
-  --     require('plugins.configs.package-info')
-  --   end,
   --   requires = 'MunifTanjim/nui.nvim',
   -- })
 
   -- LSP
   use('williamboman/mason.nvim')
   use('williamboman/mason-lspconfig.nvim')
-  use({
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    config = function()
-      require('plugins.configs.mason-tool-installer')
-    end,
-  })
+  use('WhoIsSethDaniel/mason-tool-installer.nvim')
   use({
     'neovim/nvim-lspconfig',
     after = {
@@ -49,40 +35,21 @@ packer.startup(function(use)
       'neodev.nvim',
       'schemastore.nvim',
     },
-    config = function()
-      require('plugins.configs.mason')
-      require('plugins.configs.mason-lspconfig')
-      require('plugins.configs.lspconfig')
-    end,
   })
   use({
     'jose-elias-alvarez/null-ls.nvim',
-    config = function()
-      require('plugins.configs.null-ls')
-    end,
     requires = { 'nvim-lua/plenary.nvim' },
   })
   use({
     'glepnir/lspsaga.nvim',
     after = 'nvim-lspconfig',
-    config = function()
-      require('plugins.configs.lspsaga')
-    end,
   })
-  use({
-    'j-hui/fidget.nvim',
-    config = function()
-      require('plugins.configs.fidget')
-    end,
-  })
+  use('j-hui/fidget.nvim')
 
   -- Rust
   use({
     'simrat39/rust-tools.nvim',
     after = 'nvim-lspconfig',
-    config = function()
-      require('plugins.configs.rust-tools')
-    end,
   })
 
   -- completion and snippet
@@ -91,16 +58,10 @@ packer.startup(function(use)
   use({
     'hrsh7th/nvim-cmp',
     after = { 'friendly-snippets', 'lspkind.nvim' },
-    config = function()
-      require('plugins.configs.cmp')
-    end,
   })
   use({
     'L3MON4D3/LuaSnip',
     after = 'nvim-cmp',
-    config = function()
-      require('plugins.configs.luasnip')
-    end,
   })
   use({ 'saadparwaiz1/cmp_luasnip', after = 'LuaSnip' })
   use({ 'hrsh7th/cmp-nvim-lsp', after = 'cmp_luasnip' })
@@ -111,16 +72,8 @@ packer.startup(function(use)
   use({
     'windwp/nvim-autopairs',
     after = 'nvim-cmp',
-    config = function()
-      require('plugins.configs.autopairs')
-    end,
   })
-  use({
-    'windwp/nvim-ts-autotag',
-    config = function()
-      require('plugins.configs.ts-autotag')
-    end,
-  })
+  use('windwp/nvim-ts-autotag')
 
   -- theme
   -- ['folke/tokyonight.nvim'] = {
@@ -135,104 +88,38 @@ packer.startup(function(use)
   use('kyazdani42/nvim-web-devicons')
 
   -- start page
-  use({
-    'goolord/alpha-nvim',
-    config = function()
-      require('plugins.configs.alpha')
-    end,
-  })
+  use('goolord/alpha-nvim')
 
   -- sidebar
-  use({
-    'kyazdani42/nvim-tree.lua',
-    config = function()
-      require('plugins.configs.nvim-tree')
-    end,
-  })
+  use('kyazdani42/nvim-tree.lua')
 
   -- status bar
-  use({
-    'nvim-lualine/lualine.nvim',
-    config = function()
-      require('plugins.configs.lualine')
-    end,
-  })
+  use('nvim-lualine/lualine.nvim')
 
   -- tab bar
-  use({
-    'akinsho/bufferline.nvim',
-    tag = 'v2.*',
-    config = function()
-      require('plugins.configs.bufferline')
-    end,
-  })
+  use({ 'akinsho/bufferline.nvim', tag = 'v2.*' })
 
-  use({
-    'numToStr/Comment.nvim',
-    config = function()
-      require('plugins.configs.comment')
-    end,
-  })
+  use('numToStr/Comment.nvim')
 
   -- finder
-  use({
-    'nvim-telescope/telescope.nvim',
-    config = function()
-      require('plugins.configs.telescope')
-    end,
-    requires = { 'nvim-lua/plenary.nvim' },
-  })
+  use({ 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } })
 
   -- git
-  use({
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require('plugins.configs.gitsigns')
-    end,
-  })
+  use('lewis6991/gitsigns.nvim')
 
   -- key binding sheet
-  use({
-    'folke/which-key.nvim',
-    config = function()
-      require('plugins.configs.which-key')
-    end,
-  })
+  use('folke/which-key.nvim')
 
   use({
     'folke/trouble.nvim',
     requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-      require('plugins.configs.trouble')
-    end,
   })
 
-  use({
-    'lukas-reineke/indent-blankline.nvim',
-    config = function()
-      require('plugins.configs.indent-blankline')
-    end,
-  })
+  use('lukas-reineke/indent-blankline.nvim')
 
-  use({
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('plugins.configs.colorizer')
-    end,
-  })
+  use('norcalli/nvim-colorizer.lua')
 
-  use({
-    'akinsho/toggleterm.nvim',
-    tag = 'v2.*',
-    config = function()
-      require('plugins.configs.toggleterm')
-    end,
-  })
+  use({ 'akinsho/toggleterm.nvim', tag = 'v2.*' })
 
-  use({
-    'keaising/im-select.nvim',
-    config = function()
-      require('plugins.configs.im-select')
-    end
-  })
+  use('keaising/im-select.nvim')
 end)
