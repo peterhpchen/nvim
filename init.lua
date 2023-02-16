a060a90a90868941dbb34189bccc50b9d6d3df53
peterhpchen <peterhsinpingchen@gmail.com>
2023-02-16 16:41:30 +0800

    refactor: individual diagnostic file

--- init.lua
+++ init.lua
@@ -1,11 +1,4 @@
 require('core.options')
 require('core.autocmds')
 require('plugins')
-
--- diagnostic
-vim.diagnostic.config({ severity_sort = true })
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-end
+require('core.diagnostic')
---
+++ lua/core/diagnostic.lua
@@ -0,0 +1,6 @@
+vim.diagnostic.config({ severity_sort = true })
+local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+end
