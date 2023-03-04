27488b96048c901cdf2932ced7564cfb2088cdbb
peterhpchen <peterhsinpingchen@gmail.com>
2023-03-04 21:27:24 +0800

    feat(mappings): desc

--- lua/peterhpchen/plugins/configs/telescope.lua
+++ lua/peterhpchen/plugins/configs/telescope.lua
@@ -6,14 +6,14 @@ end
 
 telescope.setup({})
 
-vim.keymap.set('n', '<leader>ff', '<cmd> Telescope find_files <CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ff', '<cmd> Telescope find_files <CR>', { noremap = true, silent = true, desc = 'Find File' })
 vim.keymap.set(
   'n',
   '<leader>fa',
   '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>',
   { noremap = true, silent = true }
 )
-vim.keymap.set('n', '<leader>fw', '<cmd> Telescope live_grep <CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fw', '<cmd> Telescope live_grep <CR>', { noremap = true, silent = true, desc = 'Find Content' })
 vim.keymap.set('n', '<leader>fb', '<cmd> Telescope buffers <CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fh', '<cmd> Telescope help_tags <CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fo', '<cmd> Telescope oldfiles <CR>', { noremap = true, silent = true })
