ad32db432185e85c624b14a52d30791e8e08dc03
peterhpchen <peterhsinpingchen@gmail.com>
2023-06-19 08:47:59 +0800

    feat(null-ls): let prettier set in the astro file

--- lua/peterhpchen/plugins/configs/null-ls.lua
+++ lua/peterhpchen/plugins/configs/null-ls.lua
@@ -40,5 +40,9 @@ null_ls.setup({
   sources = {
     -- Git
     null_ls.builtins.code_actions.gitsigns,
+
+    null_ls.builtins.formatting.prettierd.with({
+      extra_filetypes = { 'astro' },
+    }),
   },
 })
