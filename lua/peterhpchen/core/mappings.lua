880ffbcdcb0b8563f58941bec0792a96a7d53cf5
peterhpchen <peterhsinpingchen@gmail.com>
2023-12-19 10:41:20 +0800

    feat(telescope): add find buffers keymap

--- lua/peterhpchen/core/mappings.lua
+++ lua/peterhpchen/core/mappings.lua
@@ -1,15 +1,7 @@
 local keymap = vim.keymap
 
 keymap.set('n', '<Leader>x', ':bd<CR>', { desc = 'close buffer' })
-keymap.set('n', '<Leader>xa', ':%bd|e#<CR>', { desc = 'close all buffers' })
+keymap.set('n', '<Leader>xa', ':%bd|e#|bd#<CR>', { desc = 'close all buffers except current' })
 
 -- undotree
 keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'toggle undotree panel' })
-
--- trouble
-keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>', { desc = 'toggle trouble list' })
-
--- telescope
-keymap.set('n', '<leader>ff', '<cmd> Telescope find_files <CR>', { desc = 'find file' })
-keymap.set('n', '<leader>fw', '<cmd> Telescope live_grep <CR>', { desc = 'find content' })
-keymap.set('n', '<leader>fh', '<cmd> Telescope help_tags <CR>', { desc = 'find help' })
--- lua/peterhpchen/plugins/configs/telescope.lua
+++ lua/peterhpchen/plugins/configs/telescope.lua
@@ -4,6 +4,8 @@ if not present then
   return
 end
 
+local builtin = require('telescope.builtin')
+
 telescope.setup({
   defaults = {
     layout_strategy = 'vertical',
@@ -38,3 +40,8 @@ vim.keymap.set('n', '<C-n>', function()
     previewer = false,
   })
 end, { desc = 'open file browser' })
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'find file' })
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'find content' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'find buffer' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'find help' })
