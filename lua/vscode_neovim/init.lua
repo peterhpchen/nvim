bb13bde38979cb47c2560d57d70e90134747195f
peterhpchen <peterhsinpingchen@gmail.com>
2025-02-11 23:24:00 +0800

    feat(vscode): keymap for telescope

--- lua/vscode_neovim/init.lua
+++ lua/vscode_neovim/init.lua
@@ -1,2 +1,16 @@
 -- options
+vim.g.mapleader = ';' -- Default is '\'
+
 vim.opt.clipboard = 'unnamed,unnamedplus'
+
+vim.keymap.set("n", "<space>fb", function()
+  require("vscode").action("workbench.view.explorer", { })
+end, {})
+
+vim.keymap.set("n", "<leader>fg", function()
+  require("vscode").action("workbench.view.search", { })
+end, {})
+
+vim.keymap.set("n", "<leader>fb", function()
+  require("vscode").action("workbench.action.showAllEditors", { })
+end, {})
